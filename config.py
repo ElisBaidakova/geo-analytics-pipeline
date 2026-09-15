8c19cae737ef15a454ea03c55a48b450851acdb0
ElisBaidakova <baidakova2011@gmail.com>
2026-09-15 21:58:59 +0400

    Add configuration paths for data files

---
+++ config.py
@@ -0,0 +1,6 @@
+EVENTS_PATH = "/user/master/data/geo/events"
+CITIES_PATH = "/user/s26546941/data/geo/geo.csv"
+
+USER_MART_PATH = "/user/s26546941/data/marts/user_geo_mart"
+ZONE_MART_PATH = "/user/s26546941/data/marts/zone_geo_mart"
+FRIEND_REC_MART_PATH = "/user/s26546941/data/marts/friend_rec_mart"
